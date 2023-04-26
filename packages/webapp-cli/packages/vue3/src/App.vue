6950124772f2f709b9dc0111bcf74b86b5568cb3
WChenSenemail <940930362@qq.com>
2023-04-26 15:32:26 +0800

    chore:调整配置文件

--- packages/webapp-cli/packages/vue3/src/App.vue
+++ packages/webapp-cli/packages/vue3/src/App.vue
@@ -1,21 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts" name="app"></script>
 
 <template>
-  <div space-x-2 class="test" fcc>
+  <div space-x-2 class="test mx-0" fcc>
     <router-link to="/login">login</router-link>
     <router-link to="/">home</router-link>
   </div>
   <router-view />
 </template>
-
-<style scoped>
-.test {
-  display: flex;
-  align-items: center;
-  justify-items: center;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: auto auto;
-  grid-gap: 20px;
-}
-</style>
--- packages/webapp-cli/packages/vue3/src/views/About.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="mx-4">About{{ state }}</div>
-</template>
-
-<script lang="ts" setup name="About">
-const state = ref('0')
-</script>
